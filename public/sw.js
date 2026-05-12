50c8b3b05effb0c5d90464c5b6eab40d13a6a048
Lum <paolosimone05@gmail.com>
2026-05-12 15:13:53 +0200

    ti prego speriamo speriamo

---
+++ public/sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'neonchess-cache-v1';
+const urlsToCache = [
+    '/',
+    '/index.html',
+    '/style.css',
+    '/game.js',
+    '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                // Ritorna il file dalla cache se c'è, altrimenti lo scarica da internet
+                return response || fetch(event.request);
+            })
+    );
+});
